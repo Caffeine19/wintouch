848f282559167c12e647b3934ef14766767d3572
Caffeine19 <108726581+Caffeine19@users.noreply.github.com>
2023-05-12 17:09:06 +0800

    :package: 添加install.ps1

---
+++ install.ps1
@@ -0,0 +1,6 @@
+$destination="$env:LOCALAPPDATA\wintouch\wintouch_windows_amd64.exe"
+New-Item -ItemType Directory -Path "$env:LOCALAPPDATA\wintouch" -Force
+Copy-Item -Path "./wintouch_windows_amd64.exe" -Destination $destination -Force
+
+$newPath = $envPath + ";$destination"
+[System.Environment]::SetEnvironmentVariable("Path", $newPath, "Machine")
