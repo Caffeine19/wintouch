f322fe45db8dc08253b044dc22b8cfd88bee9326
Caffeine19 <939597201@qq.com>
2023-05-15 11:43:42 +0800

    :bug: 修复安装时候把环境变量搞没的问题

--- install.ps1
+++ install.ps1
@@ -2,7 +2,8 @@ $destination="$env:LOCALAPPDATA\wintouch\wintouch_windows_amd64.exe"
 New-Item -ItemType Directory -Path "$env:LOCALAPPDATA\wintouch" -Force
 Copy-Item -Path "./wintouch_windows_amd64.exe" -Destination $destination -Force
 
-$newPath = $envPath + ";$destination"
+$newPath = $env:Path + ";$destination"
+
 [System.Environment]::SetEnvironmentVariable("Path", $newPath, "Machine")
 
- NEw-Alias -Name touch -Value wintouch_windows_amd64.exe
+New-Alias -Name touch -Value wintouch_windows_amd64.exe
